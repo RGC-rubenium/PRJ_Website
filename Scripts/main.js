3372146233b9bf7034a06a851ccba704422e7886
RGC-rubenium <coponraven@gmail.com>
2025-05-11 22:42:17 +0800

    hold version

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,8 +1,10 @@
 function openForm(){
     document.getElementById("AddProduct_Form").style.display = "flex";
+    document.getElementById("mob_nav").style.display="none";
 }
 function closeForm(){
     document.getElementById("AddProduct_Form").style.display = "none";
+    document.getElementById("mob_nav").style.display="flex";
 }
 
 window.onclick = function(event){
@@ -10,4 +12,12 @@ window.onclick = function(event){
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+//
+function openMenu(){
+    document.getElementById("page_menu").style.display = "block";
+}
+function closeMenu(){
+    document.getElementById("page_menu").style.display = "none";
 }
